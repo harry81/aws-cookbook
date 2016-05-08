b2d1699039c428b8d64148ae1adca17647486949
Hyunmin Choi <pointer81@gmail.com>
2016-05-08 18:53:30 +0200

    postdam_load.py

--- news/recipes/deploy.rb
+++ news/recipes/deploy.rb
@@ -1,3 +1,15 @@
+bash "migrate" do
+  user "deploy"
+  cwd node.news.deploy_path
+  code <<-BASH
+  export HOME=/home/deploy
+  source venv/bin/activate
+  cd src/news/backend
+  python manage.py migrate
+  BASH
+  action :run
+end
+
 bash "run uwsgi" do
   user "deploy"
   cwd node.news.deploy_path
