1884fe3256f57905e97de6474bb9ae499867a6bb
Hyunmin Choi <pointer81@gmail.com>
2016-05-21 10:59:15 +0200

    created new cookbook for hoodpub

---
+++ hoodpub/metadata.rb
@@ -0,0 +1,7 @@
+name 'hoodpub'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures hoodpub'
+long_description 'Installs/Configures hoodpub'
+version '0.1.0'
